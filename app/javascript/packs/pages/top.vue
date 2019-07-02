9178dd41b074c3e833cf4fa3b371a230d26fc5ff
yuya <siren.night43@gmail.com>
2019-07-02 17:26:56 +0900

    Fix top text

--- app/javascript/packs/pages/top.vue
+++ app/javascript/packs/pages/top.vue
@@ -2,15 +2,21 @@
   <div>
     <h1>お気に入りのスペースを登録しよう！</h1>
     <p>誰かに進めたい場所はありませんか？</p>
+    <description context="デートする時のおすすめスポットを知りたい！"
+                 text="ネットで調べてもどのあたりなのかいまいちわからない。。。">
+      
+    </description>
   </div>
 </template>
 
 <script>
   import Header from '../compoents/header';
+  import Description from "../compoents/description";
   
   export default {
     name: 'Top',
     components: {
+      Description,
       Header
     },
   }
